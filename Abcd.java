1a94fb30242c5fc9787e0c0a86a533f6292604ad
alamgirsu <alamgirsu2526@gmail.com>
2025-09-08 21:16:51 +0600

    Add files via upload

---
+++ Abcd.java
@@ -0,0 +1,21 @@
+
+package abcd;
+
+public class Abcd {
+
+
+    public static void main(String[] args) 
+    {
+        char i = 'a' ;
+        System.out.println("Enter the value ");
+       do
+        {
+              System.out.println("  "+i);
+              i++;
+           
+        }
+       while(i<='z');
+    
+    }
+    
+}
