e11ddbd695934c7c0767d6ccf54b03a106a7bb4a
alamgirsu <alamgirsu2526@gmail.com>
2025-09-08 21:04:15 +0600

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+
+package multiplication;
+
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+    int num = 5;
+    int i = 1;
+    System.out.println("Enter the Multiple ");
+    for(i = 1; i<=10; i++)
+    {
+      System.out.println(num + "X" +i+ "=" + num*i);
+    }
+    }
+}
