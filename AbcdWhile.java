e943b2dcc89849fb0dedaf3cd0bdb2af79528a44
alamgirsu <alamgirsu2526@gmail.com>
2025-09-08 21:15:06 +0600

    Add files via upload

---
+++ AbcdWhile.java
@@ -0,0 +1,18 @@
+
+package abcdwhile;
+
+
+public class AbcdWhile {
+
+   
+    public static void main(String[] args) {
+        char i = 'a';
+        System.out.print("Enter the  letter ");
+        while(i < 'z')
+        {
+            i++;
+           System.out.print(" " +i);
+        }
+    }
+    
+}
